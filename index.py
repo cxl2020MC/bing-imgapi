91c9b2ab382fb6812e89123acd0c5dd7d742a116
cxl2020MC <andamu2019@163.com>
2022-06-30 06:50:45 +0800

    vscode update

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 import requests
 app = Flask(__name__)
 
 @app.route('/bing/')
-def index():
+def bing():
    # https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN
-   url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+   idx = request.args.get('idx')
+   if idx != None:
+      url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=' + idx + '&n=1&mkt=zh-CN'
+   else:
+      url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+   print('url:', url)
    data = requests.get(url).json()
    print(data)
    imgurl = data["images"][0]["url"]
