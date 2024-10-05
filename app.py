6cbc8e01a2d9e04a8d81e4db7e0f1f333d887788
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-10-05 12:55:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(idx=1, uhd=None):
+   returl = api.bing(idx, uhd)
+   return RedirectResponse(returl)
+   elif info == 'info':
+      return api.info()
+   else:
+      return redirect('https://api.cxl2020mc.top?api=bing')
