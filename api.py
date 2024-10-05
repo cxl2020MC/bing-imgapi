806b6db1fd0e90bb6493dffb67e279f47ec8220c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-10-05 05:37:13 +0000

    test

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-import asyncio
+import aiohttp
 import time
 
 api_url = 'https://cn.bing.com/HPImageArchive.aspx'
@@ -19,26 +19,3 @@ async def bing(idx, UHD):
             returl = 'https://cn.bing.com' + imgurl
             print('imgurl:', returl)
             return returl
-
-
-def info():
-    cn_url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-    en_url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-    # print('url:', url)
-    开始时间 = time.time()
-    data = requests.get(cn_url).json()
-    结束时间 = time.time()
-    print(data)
-    中国节点用时 = 结束时间 - 开始时间
-    开始时间 = time.time()
-    data = requests.get(en_url).json()
-    结束时间 = time.time()
-    print(data)
-    全球节点用时 = 结束时间 - 开始时间
-    msg = '''info：
-bing壁纸api请求用时
-
-中国: {中国节点用时} s
-
-全球: {全球节点用时} s'''.format(中国节点用时=中国节点用时, 全球节点用时=全球节点用时)
-    return msg
